604bdf5a27c0f8838281b4ea6666c29aab2c738d
Frank-197 <frankomosh197@gmail.com>
2022-06-05 23:02:21 +0300

    Image

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "JavaScript";
 document.querySelector("body").appendChild(h2);
+
+element.addEventListener("click", function(){
+     alert("Hello World!"); 
+});
+document.getElementById("button").addEventListener("click", displayDate);
